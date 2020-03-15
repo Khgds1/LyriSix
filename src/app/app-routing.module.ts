666657fdfca0b44230d0a97b72a55f33559afd98
Khgds1 <khgds1@hotmail.com>
2020-03-14 23:18:14 -0300

    adicionando rotas e navegacao via menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent} from './pages/home/home.component';
+import { ArtistsComponent} from './pages/artists/artists.component';
+import { SongsComponent} from './pages/songs/songs.component';
+import { ProfileComponent} from './pages/profile/profile.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'artists', component: ArtistsComponent},
+  { path: 'songs', component: SongsComponent},
+  { path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
